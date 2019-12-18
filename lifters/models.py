979bbf9f8f688ee3875b5878be94606b71eaa222
z5255794 <z5255794@cse.unsw.edu.au>
2019-12-18 17:10:25 +1100

    create test data

--- lifters/models.py
+++ lifters/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
 class Athlete(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=60)
     date_of_birth = models.DateField()
     gender = models.BooleanField()
     bodyweight = models.FloatField()
     affiliation = models.CharField(max_length=3)
 
     def __str__(self):
-        return self.last_name
+        return self.name
     
     def get_birth_year(self):
         return self.date_of_birth.year
@@ -20,5 +19,5 @@ class Lift(models.Model):
     attempt = models.IntegerField()
     
     def __str__(self):
-        return self.weight
+        return str(self.weight)
     
--- lifters/views.py
+++ lifters/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
 
 def results(request):
     ''' Return results of all athletes in a weightclass '''
-    pass
+    return HttpResponse("Lol!")
 
 def update(request):
     ''' Update athlete attempts '''
