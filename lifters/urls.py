ca1381f5f21e3fd4c17dec74d04fe8df6e5a7692
boat man <z5255794@gitlab.cse.unsw.EDU.AU>
2019-12-23 12:55:32 +1100

    add create athlete functionality

--- lifters/urls.py
+++ lifters/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from lifters.views import *
 
+app_name='lifters'
+
 urlpatterns = [
-    path('', results),
-    path('admin/', admin),
-    path('create/', create)
+    path('', results, name='results'),
+    path('admin/', admin, name='admin'),
+    path('create/', create, name='create')
 ]
--- lifters/views.py
+++ lifters/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
 from .models import Athlete, Lift
 
@@ -8,16 +9,24 @@ from .models import Athlete, Lift
 def admin(request):
     ''' Admin page for meets '''
     
-    return render(request, 'lifters/admin.html')
+    # Return list of dictionaries
+    athletes_list = list(Athlete.objects.all().values())
+
+    context = {
+        'athletes_list': athletes_list
+    }
+
+
+    return render(request, 'lifters/admin.html', context)
 
 def results(request):
     ''' Return results of all athletes in a weightclass '''
 
     # Return list of dictionaries
-    lifters_list = list(Athlete.objects.all().values())
+    athletes_list = list(Athlete.objects.all().values())
 
     context = {
-        'lifters_list': lifters_list
+        'athletes_list': athletes_list
     }
 
     return render(request, 'lifters/index.html', context)
@@ -28,7 +37,18 @@ def update(request):
 
 def create(request):
     ''' Create athlete profile '''
-    return HttpResponse("This works!")
+
+    new_athlete = Athlete(
+        name = request.POST['name'],
+        date_of_birth = request.POST['date_of_birth'],
+        gender = True if request.POST['gender'] == "Male" else False,
+        bodyweight = request.POST['bodyweight'],
+        affiliation = request.POST['affiliation'].upper()
+    )
+
+    new_athlete.save()
+
+    return HttpResponseRedirect(reverse('lifters:admin'))
 
 def delete(request):
     ''' Delete athlete profile '''
