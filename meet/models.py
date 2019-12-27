4e12e8aec130c175f2365bae9e71b27881a77343
Andy Bui <andy.bui2001@gmail.com>
2019-12-27 17:08:17 +1100

    add user-meet relationship

--- meet/models.py
+++ meet/models.py
@@ -2,6 +2,10 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 class Meet(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=100)
 
     def __str__(self):
