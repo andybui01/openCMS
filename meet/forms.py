f0faff5b17815cead352b540a4a0816068d09ad8
Andy Bui <andy.bui2001@gmail.com>
2019-12-28 14:05:04 +1100

    remove placeholder text

--- meet/forms.py
+++ meet/forms.py
@@ -3,14 +3,12 @@ from django import forms
 class SessionForm(forms.Form):
     bodyweight = forms.IntegerField(
         label="Bodyweight:",
-        required=True,
-        widget=forms.NumberInput(attrs={'placeholder': '81'}))
+        required=True)
     letter = forms.CharField(
         label="Letter:",
         max_length=1,
-        required=True,
-        widget=forms.TextInput(attrs={'placeholder': 'A'}))
+        required=True)
     start_time = forms.TimeField(
-        label="Start Time (HH:MM):",
+        label="Start Time:",
         required=True,
-        widget=forms.TimeInput(attrs={'placeholder': '13:00'}))
+        help_text="HH:MM")
